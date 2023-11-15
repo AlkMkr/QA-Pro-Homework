d41b03278a3e1da60753b9de58b6857daa05efb4
AlkMkr <alekseymkaale@gmail.com>
2023-11-15 16:58:00 +0200

    Fixing

--- Lesson4_1.js
+++ Lesson4_1.js
@@ -1,17 +1,21 @@
 const prompt = require("prompt-sync")();
 let unit = prompt("Enter the unit: km, kg, h: ");
 let value_of_unit = prompt("Enter the value: ");
-if (value_of_unit) {
+let converted;
+if (value_of_unit) { 
   if (value_of_unit % 1 === 0) {
     switch (unit) {
       case "km":
-        console.log(`${value_of_unit}km is ${value_of_unit * 1000}m`);
+        converted = value_of_unit * 1000;
+        console.log(`${value_of_unit}km is ${converted}m`);
         break;
       case "kg":
-        console.log(`${value_of_unit}kg is ${value_of_unit * 1000}g`);
+        converted = value_of_unit * 1000;
+        console.log(`${value_of_unit}kg is ${converted}g`);
         break;
       case "h":
-        console.log(`${value_of_unit} hours is ${value_of_unit * 60} minutes`);
+        converted = value_of_unit * 60;
+        console.log(`${value_of_unit} hours is ${converted} minutes`);
         break;
       default:
         console.log(`${unit} isn't supported by converter`);
