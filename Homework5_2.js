164b7266f38eadef1701ab0eef3d3a8bc793849e
AlkMkr <alekseymkaale@gmail.com>
2023-11-20 16:35:27 +0200

    remaking methods to object methods

--- Homework5_2.js
+++ Homework5_2.js
@@ -3,41 +3,46 @@ var services = {
   "repair glass": "50 UAH",
   haircut: "100 UAH",
   "exquisite haircut": "200 UAH",
-  "beard trimming" : "100 UAH"
+  "beard trimming": "100 UAH",
+  price: function () {
+    let sum = 0;
+    for (let key in this) {
+      let num = parseFloat(this[key]);
+      if (!isNaN(num)) {
+        sum += num;
+      }
+    }
+    console.log(sum);
+  },
+  minPrice: function() {
+    let min = Infinity; // I kinda need this
+    for (let key in this) {
+      let num = parseFloat(this[key]);
+      if (num < min) {
+        min = num;
+      }
+    }
+    console.log(min);
+  },
+  maxPrice: function() {
+    let max = 0;
+    for (let key in this) {
+      let num = parseFloat(this[key]);
+      if (num > max) {
+        max = num;
+      }
+    }
+    console.log(max);
+  }
 };
 
-function price(obj) {
-  let sum = 0;
-  for (let key in obj) {
-    sum += parseFloat(obj[key]);
-  }
-  return sum;
-}
 
-function minPrice(obj) {
-  let min = Infinity; // I kinda need this
-  for (let key in obj) {
-    let num = parseFloat(obj[key]);
-    if (num < min) {
-      min = num;
-    }
-  }
-  return min;
-}
-function maxPrice(obj){
-    let max = 0;
-    for (let key in obj){
-        let num = parseFloat(obj[key]);
-        if (num > max){
-            max = num
-        }
-    }
-    return max;
-}
 
-const pricing = price(services);
-console.log(pricing);
-const minimum = minPrice(services);
-console.log(minimum);
-const maximum = maxPrice(services);
-console.log(maximum);
+services.price();
+services.minPrice();
+services.maxPrice();
+services['drive around'] = '10 UAH';
+services['drive in lamborgini'] = '1000 UAH';
+services.price();
+services.minPrice();
+services.maxPrice();
