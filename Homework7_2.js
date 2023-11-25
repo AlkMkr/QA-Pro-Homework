a54420b2e1549defec2cefd900fc7926450fd5bf
AlkMkr <alekseymkaale@gmail.com>
2023-11-25 15:58:25 +0200

    Homework 7

---
+++ Homework7_2.js
@@ -0,0 +1,32 @@
+var arr = [
+  {
+    userName: "Test",
+    lastName: "Test",
+    email: "test.test@gmail.com",
+  },
+  {
+    userName: "Dmitro",
+    lastName: "Porohov",
+    email: "dmitro.porohov@yahoo.com",
+  },
+  {
+    userName: "Andrii",
+    lastName: "",
+    email: "andrii@mail.ru", // Нам такі не підходять
+  },
+];
+
+function filterEmail(arr) {
+  let reg = /^[^\s@]+@(gmail|yahoo)\.com$/i;
+  let filter = [];
+  for (let obj = 0; obj < arr.length; obj++) {
+    let check = arr[obj];
+    if (reg.test(check.email)) {
+      filter.push(check);
+    }
+  }
+  return filter;
+}
+
+const filter = filterEmail(arr);
+console.log(filter);
